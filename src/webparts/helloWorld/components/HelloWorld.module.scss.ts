16b57ac3decbede84fcb7d0890ac7a0d3d2b16f7
BooJAVA <58940030+BooJAVA@users.noreply.github.com>
2020-02-25 18:01:47 +0200

    upload
    upload

---
+++ src/webparts/helloWorld/components/HelloWorld.module.scss.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+require("./HelloWorld.module.css");
+const styles = {
+  helloWorld: 'helloWorld_6efd522c',
+  container: 'container_6efd522c',
+  row: 'row_6efd522c',
+  column: 'column_6efd522c',
+  'ms-Grid': 'ms-Grid_6efd522c',
+  title: 'title_6efd522c',
+  subTitle: 'subTitle_6efd522c',
+  description: 'description_6efd522c',
+  button: 'button_6efd522c',
+  label: 'label_6efd522c',
+  list: 'list_6efd522c',
+  listItem: 'listItem_6efd522c'
+};
+
+export default styles;
+/* tslint:enable */
